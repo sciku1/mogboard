a764af8c904134a2e85823f3fe94ddcbfa189646
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-03-09 22:03:45 -0700

    Fix ClassJob abbreviations

---
+++ CoinachExporter/MogboardDataExporter/Data/ClassJobMap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace MogboardDataExporter.Data
+{
+    public static class Resources
+    {
+        public static Dictionary<string, string> ClassJobMap = new Dictionary<string, string>
+        {
+            { "GLA", "PLD" },
+            { "MRD", "WAR" },
+            { "CNJ", "WHM" },
+            { "PGL", "MNK" },
+            { "LNC", "DRG" },
+            { "ROG", "NIN" },
+            { "ARC", "BRD" },
+            { "THM", "BLM" },
+            { "ACN", "SMN" },
+        };
+    }
+}
