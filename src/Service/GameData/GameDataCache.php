d9fb046a676e3c1cedf96edc0185c84233af482a
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-07-02 10:34:44 -0700

    make the mappings load as an array

--- src/Service/GameData/GameDataCache.php
+++ src/Service/GameData/GameDataCache.php
@@ -143,7 +143,7 @@ class GameDataCache
         $categories = [];
         $categoriesFull = [];
 
-        $chsIscMappings = \json_decode(\file_get_contents('DataExports/ItemSearchCategory_Mappings_Chs.json'));
+        $chsIscMappings = \json_decode(\file_get_contents('DataExports/ItemSearchCategory_Mappings_Chs.json'), true);
     
         foreach ($objects as $category) {
             // ignore empty ones
