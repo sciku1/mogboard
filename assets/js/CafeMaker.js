f13969452b0309a1a623604a79c32df1b6fddc99
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-07-02 15:47:25 -0700

    undo the thing

--- assets/js/CafeMaker.js
+++ assets/js/CafeMaker.js
@@ -1,4 +1,6 @@
-class CafeMaker
+import XIVAPI from "./XIVAPI";
+
+class CafeMaker extends XIVAPI
 {
     get(endpoint, queries, callback)
     {
@@ -13,93 +15,6 @@ class CafeMaker
         fetch(`https://cafemaker.wakingsands.com${endpoint}`, { mode: 'cors' })
             .then(response => response.json())
             .then(callback)
-    }
-
-    /**
-     * Search for an item
-     */
-    search(string, callback) {
-        let params = {
-            indexes: 'item',
-            filters: 'ItemSearchCategory.ID>=1',
-            columns: 'ID,Icon,Name,LevelItem,Rarity,ItemSearchCategory.Name,ItemSearchCategory.ID,ItemKind.Name',
-            string:  string.trim(),
-            limit:   100,
-            sort_field: 'LevelItem',
-            sort_order: 'desc'
-        };
-
-        this.get(`/search`, params, callback);
-    }
-
-    /**
-     * A limited search
-     */
-    searchLimited(string, callback) {
-        let params = {
-            indexes: 'item',
-            filters: 'ItemSearchCategory.ID>=1',
-            columns: 'ID,Name',
-            string:  string.trim(),
-            limit:   10,
-        };
-
-        this.get(`/search`, params, callback);
-    }
-
-    /**
-     * Search for a character
-     */
-    searchCharacter(name, server, callback) {
-        this.get(`/character/search`, {
-            name: name,
-            server: server
-        }, callback);
-    }
-
-    /**
-     * Get a specific character
-     */
-    getCharacter(lodestoneId, callback) {
-        this.get(`/character/${lodestoneId}`, {}, callback);
-    }
-
-    /**
-     * Confirm character verification state
-     */
-    characterVerification(lodestoneId, token, callback) {
-        this.get(`/character/${lodestoneId}/verification`, {
-            token: token
-        }, callback);
-    }
-
-    /**
-     * Return information about an item
-     */
-    getItem(itemId, callback) {
-        this.get(`/Item/${itemId}`, {}, callback);
-    }
-
-    /**
-     * Get a list of servers grouped by their data center
-     */
-    getServerList(callback) {
-        fetch(`https://universalis.app/json/dc.json`, { mode: 'cors' })
-            .then(response => response.json())
-            .then(callback)
-    }
-
-    /**
-     *
-     */
-    getMarketPrices(itemId, server, callback)
-    {
-        const options = {
-            columns: 'Prices,Item',
-            servers: server,
-        };
-
-        this.get(`/market/item/${itemId}`, options, callback);
     }
 }
 
