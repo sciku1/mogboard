c7852952e65e0bdbea5a5c711a83bebbb04513de
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-07-04 10:15:12 -0700

    make the categories ordered properly

--- src/Twig/HackyExtension.php
+++ src/Twig/HackyExtension.php
@@ -26,32 +26,26 @@ class HackyExtension extends AbstractExtension
                 'items' => [],
                 'housing' => [],
             ];
+            $weaponsCatIds = [10, 11, 76, 86, 13, 9, 83, 73, 12, 77, 87, 14, 16, 84, 15, 85, 78, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+            $armorCatIds = [17, 31, 33, 36, 38, 35, 37, 40, 39, 41, 42];
+            $itemsCatIds = [43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 57, 58, 59, 60, 74, 75, 79, 80];
+            $housingCatIds = [65, 66, 67, 56, 68, 69, 70, 71, 72, 81, 82];
             $data = json_decode(file_get_contents('../DataExports/ItemSearchCategory_Mappings_Chs.json'), TRUE);
-            foreach ($data as $i => $isc) {
-                if (in_array($i, [10, 11, 76, 86, 13, 9, 83, 73, 12, 77, 87, 14, 16, 84, 15, 85, 78, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30])) {
-                    array_push($retObj['weapons'], [
-                        'ID' => ''.$i,
-                        'Name' => $isc,
-                    ]);
-                } else if (in_array($i, [17, 31, 33, 36, 38, 35, 37, 40, 39, 41, 42])) {
-                    array_push($retObj['armor'], [
-                        'ID' => ''.$i,
-                        'Name' => $isc,
-                    ]);
-                } else if ($i >= 43 and $i <= 60 and $i !== 56 or in_array($i, [74, 75, 79, 80])) {
-                    array_push($retObj['items'], [
-                        'ID' => ''.$i,
-                        'Name' => $isc,
-                    ]);
-                } else if (in_array($i, [65, 66, 67, 56, 68, 69, 70, 71, 72, 81, 82])) {
-                    array_push($retObj['housing'], [
-                        'ID' => ''.$i,
-                        'Name' => $isc,
-                    ]);
-                }
-            }
+            pushToChsIscArray($weaponsCatIds, $retObj['weapons'], $data);
+            pushToChsIscArray($armorCatIds, $retObj['armor'], $data);
+            pushToChsIscArray($itemsCatIds, $retObj['items'], $data);
+            pushToChsIscArray($housingCatIds, $retObj['housing'], $data);
             return $retObj;
         }
         return $obj;
+    }
+
+    private function pushToChsIscArray($inarray, $outarray, $data) {
+        foreach ($inarray as $i) {
+            array_push($outarray, [
+                'ID' => ''.$i,
+                'Name' => $data[$i],
+            ]);
+        }
     }
 }
