9c87fc4fe40b4db6011f54e8e7463bed930ffc26
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-07-05 13:37:51 -0700

    begin installation translations

---
+++ src/Twig/TranslationExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Twig;
+
+use App\Common\Service\Redis\Redis;
+use App\Common\Utils\Language;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class TranslationExtension extends AbstractExtension {
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('translate', [$this, 'translate']),
+        ];
+    }
+
+    /**
+     * Pulls a translation from the translation store using the provided key, falling back to the input text
+     * if no data is available.
+     */
+    public function translate($text, $key): string
+    {
+        $lang = Language::current();
+        $result = Redis::Cache()->get('translation_'.$key.'_'.$lang);
+        return $result ?? $text;
+    }
+}
