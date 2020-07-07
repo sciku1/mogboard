485f358325716e58882d3a86f424ba434b4a92a3
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-07-06 21:36:37 -0700

    move things around

--- src/Command/I18n/ImportTranslationsCommand.php
+++ src/Command/I18n/ImportTranslationsCommand.php
@@ -3,7 +3,7 @@
 namespace App\Command\I18n;
 
 use App\Command\CommandConfigureTrait;
-use App\Common\Service\Redis\Redis;
+use App\Service\I18n\TranslationDataCache;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,50 +12,25 @@ class ImportTranslationsCommand extends Command
 {
     use CommandConfigureTrait;
 
-    // cache to 2030
-    const CACHE_TIME = 1890691200;
-
     const COMMAND = [
         'name' => 'ImportTranslationsCommand',
         'desc' => 'Imports translations from the translations folder.',
     ];
 
-    public function __construct($name = null)
+    /**
+     * @var TranslationDataCache
+     */
+    private $tdc;
+
+    public function __construct(TranslationDataCache $tdc, string $name = null)
     {
+        $this->tdc = $tdc;
+
         parent::__construct($name);
     }
     
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $translationPaths = [
-            'chs' => 'translations/Universalis_Chinese_simplified.json',
-            'ja' => 'translations/Universalis_Japanese.json',
-            'fr' => 'translations/Universalis_French.json',
-            'de' => 'translations/Universalis_German.json',
-        ];
-        
-        foreach ($translationPaths as $lang => $path) {
-            if (!\file_exists($path)) {
-                continue;
-            }
-
-            $translationSet = \json_decode(\file_get_contents($path));
-
-            $this->console->writeln('>> Caching translations ('.$lang.')');
-
-            foreach ($translationSet as $term) {
-                if (isNullOrEmpty($term)) {
-                    continue;
-                }
-
-                Redis::Cache()->set('translation_'.$term->context.'_'.$lang, $term->definition, self::CACHE_TIME);
-            }
-        }
-
-        $this->console->writeln('Done!');
-    }
-
-    private function isNullOrEmpty($str) {
-        return (!isset($str) || trim($str) === '');
+        $this->tdc->populate();
     }
 }
---
+++ src/Service/I18n/TranslationDataCache.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Service\I18n;
+
+use App\Common\Service\Redis\Redis;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+class TranslationDataCache
+{
+    // cache to 2030
+    const CACHE_TIME = 1890691200;
+
+    /** @var ConsoleOutput */
+    private $console;
+
+    public function __construct()
+    {
+        $this->console = new ConsoleOutput();
+    }
+
+    public function populate() {
+        $this->cacheTranslations();
+    }
+    
+    private function cacheTranslations()
+    {
+        $translationPaths = [
+            'chs' => 'translations/Universalis_Chinese_simplified.json',
+            'ja' => 'translations/Universalis_Japanese.json',
+            'fr' => 'translations/Universalis_French.json',
+            'de' => 'translations/Universalis_German.json',
+        ];
+        
+        foreach ($translationPaths as $lang => $path) {
+            if (!\file_exists($path)) {
+                continue;
+            }
+
+            $translationSet = \json_decode(\file_get_contents($path));
+
+            $this->console->writeln('>> Caching translations ('.$lang.')');
+
+            foreach ($translationSet as $term) {
+                if (isNullOrEmpty($term)) {
+                    continue;
+                }
+
+                Redis::Cache()->set('translation_'.$term->context.'_'.$lang, $term->definition, self::CACHE_TIME);
+            }
+        }
+
+        $this->console->writeln('Done!');
+    }
+
+    private function isNullOrEmpty($str) {
+        return (!isset($str) || trim($str) === '');
+    }
+}
