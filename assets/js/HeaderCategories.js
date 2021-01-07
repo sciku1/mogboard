05d0ee39a8b320666fcb3b340415768b52daba48
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-01-06 17:38:49 -0800

    Really fix category icons

--- assets/js/HeaderCategories.js
+++ assets/js/HeaderCategories.js
@@ -76,7 +76,7 @@ class HeaderCategories
             items.forEach(item => {
                 const id     = item[0];
                 const name   = item[1];
-                const icon   = 'https://xivapi.com/i/0' + Math.floor(id / 1000) * 1000 + '/0' + id + '.png';
+                const icon   = 'https://xivapi.com/i' + item[2];
                 const ilv    = item[3];
                 const rarity = item[4];
                 const url    = mog.url_item.replace('-id-', id);
