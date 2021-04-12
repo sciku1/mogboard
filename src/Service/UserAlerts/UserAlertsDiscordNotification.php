9a99afdf33b9cd10efb181179a476b18fd8fda3f
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-11 18:02:53 -0700

    Copy fields back in for the reasons list

--- src/Service/UserAlerts/UserAlertsDiscordNotification.php
+++ src/Service/UserAlerts/UserAlertsDiscordNotification.php
@@ -4,6 +4,7 @@ namespace App\Service\UserAlerts;
 
 use App\Common\Entity\UserAlert;
 use App\Common\Service\Redis\Redis;
+use Carbon\Carbon;
 
 class UserAlertsDiscordNotification
 {
@@ -16,16 +17,45 @@ class UserAlertsDiscordNotification
     {
         $item = Redis::Cache()->get("xiv_Item_{$alert->getItemId()}");
 
+        $reasons = [];
+        foreach($triggeredMarketRows as $i => $marketRow) {
+            [$server, $row] = $marketRow;
+
+            $name = sprintf(
+                "%sx %s Gil - Total: %s",
+                number_format($row->Quantity),
+                number_format($row->PricePerUnit),
+                number_format($row->PriceTotal),
+                $row->IsHQ ? "HQ" : "NQ",
+                $alert->getTriggerType() == "Prices" ? $row->RetainerName : $row->CharacterName
+            );
+
+            $purchaseDate = null;
+            if ($alert->getTriggerType() == "History") {
+                $carbon = Carbon::createFromTimestamp($row->PurchaseDate);
+                $purchaseDate = $carbon->fromNow();
+            }
+
+            $value = sprintf(
+                "%s - %s - %s",
+                "({$server})",
+                $alert->getTriggerType() == "Prices" ? "Retainer: {$row->RetainerName}" : "Buyer: {$row->CharacterName}",
+                $alert->getTriggerType() == "Prices" ? "Signature: {$row->CreatorSignatureName}" : "Purchased: {$purchaseDate}"
+            );
+
+            $reasons[$i] = $name . " " . $value;
+        }
+
         $notificationInfo = [
             "itemName"      => $item->Name_en,
             "itemIcon"      => "https://xivapi.com{$item->Icon}",
             "pageUrl"       => getenv("SITE_CONFIG_DOMAIN") . "/market/{$item->ID}",
-            "reasons"       => [], // TODO
+            "reasons"       => $reasons,
         ];
         
         postResource(ALERTS_SERVICE_ENDPOINT, [
-            'targetUser'   => $alert->getUser()->getSsoDiscordId(),
-            'notification' => $notificationInfo,
+            "targetUser"   => $alert->getUser()->getSsoDiscordId(),
+            "notification" => $notificationInfo,
         ]);
     }
 
